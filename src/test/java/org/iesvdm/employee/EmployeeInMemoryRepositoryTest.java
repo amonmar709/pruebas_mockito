7b7303d25bdd15b1d78aecbaf542dff5e8d8ebae
Alicia <amonmar709@g.educaand.es>
2024-04-29 23:36:24 +0200

    Alicia Moncayo. Add code

--- src/test/java/org/iesvdm/employee/EmployeeInMemoryRepositoryTest.java
+++ src/test/java/org/iesvdm/employee/EmployeeInMemoryRepositoryTest.java
@@ -6,9 +6,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.util.SystemPropertiesPropertySource;
 import org.assertj.core.api.Assertions;
+import org.iesvdm.dominio.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 /**
  * Test doubles that are "fakes" must be tested
@@ -36,7 +39,16 @@ public class EmployeeInMemoryRepositoryTest {
 	 */
 	@Test
 	public void testEmployeeRepositoryFindAll() {
+		Employee empleado1 = new Employee("01", 1200);/** Creo los dos empleados **/
+		Employee empleado2 = new Employee("02", 1500);
 
+		employees.add(empleado1);/** Los añado a la coleccion de employees **/
+		employees.add(empleado2);
+
+		/** Por ultimo compruebo que cuando llamo a employeeRepository.findAll obtengo los empleados creados **/
+		assertThat(employeeRepository.findAll().size()).isEqualTo(2);
+		assertThat(employeeRepository.findAll().contains(empleado1) /*&& employeeRepository.findAll().contains(empleado2)*/).isTrue();
+		assertThat(employeeRepository.findAll().contains(empleado2)).isTrue();
 	}
 
 	/**
@@ -47,6 +59,14 @@ public class EmployeeInMemoryRepositoryTest {
 	 */
 	@Test
 	public void testEmployeeRepositorySaveNewEmployee() {
+		Employee empleado3 = new Employee("03", 3000);/** Creo un empleado**/
+
+		employees.add(empleado3);/** Lo añado a la colección de employees **/
+
+		employeeRepository.save(empleado3); /** Salvo al employee mediante el metodo employeeRepository.save **/
+
+		/** Por último compruebo que el array tiene un sólo elemento y que ese elemento es empleado3 **/
+		assertThat( employees.size() == 1 && employees.contains(empleado3)).isTrue();
 
 	}
 
@@ -61,6 +81,19 @@ public class EmployeeInMemoryRepositoryTest {
 	 */
 	@Test
 	public void testEmployeeRepositorySaveExistingEmployee() {
+		Employee empleado1 = new Employee("01", 1500);/** Cojo los empleados de antes con cambios en el salario **/
+		Employee empleado2 = new Employee("02", 1800);
+		Employee empleado3 = new Employee("03", 3300);
 
+		employees.add(empleado1);/** Los añado a la colección de employees **/
+		employees.add(empleado2);
+		employees.add(empleado3);
+
+		employeeRepository.save(empleado1); /** Salvo a los employee mediante el metodo employeeRepository.save **/
+		employeeRepository.save(empleado2);
+		employeeRepository.save(empleado3);
+
+		/** Por último compruebo que los datos de los empleados se mantienen actualizados **/
+		assertThat( empleado1.getSalary() == 1500 && empleado2.getSalary() == 1800 && empleado3.getSalary() == 3300).isTrue();
 	}
 }
--- src/test/java/org/iesvdm/employee/EmployeeManagerTest.java
+++ src/test/java/org/iesvdm/employee/EmployeeManagerTest.java
@@ -8,12 +8,10 @@ import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
+import org.mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeeManagerTest {
 
@@ -25,7 +23,8 @@ public class EmployeeManagerTest {
 
 	/**
 	 * Explica en este comentario que efecto tiene
-	 * esta anotacion @InjectMocks
+	 * esta anotacion @InjectMocks: esta anotación , crea el objeto simulado de
+	 * la clase e inyecta los objectos simulados que están marcados con la anotación @Mock en él.
 	 */
 	@InjectMocks
 	private EmployeeManager employeeManager;
@@ -56,6 +55,11 @@ public class EmployeeManagerTest {
 	 */
 	@Test
 	public void testPayEmployeesReturnZeroWhenNoEmployeesArePresent() {
+		/** Compruebo que devuelve una colección vacia **/
+		Mockito.when(employeeRepository.findAll()).thenReturn(new ArrayList<>());
+
+		/** Compruebo que no se paga a ningún empleado **/
+		assertThat(employeeManager.payEmployees()).isEqualTo(0);
 
 	}
 
@@ -71,6 +75,19 @@ public class EmployeeManagerTest {
 	 */
 	@Test
 	public void testPayEmployeesReturnOneWhenOneEmployeeIsPresentAndBankServicePayPaysThatEmployee() {
+		/** Me creo un ArrayList con un solo empleado **/
+		ArrayList<Employee> empleados = new ArrayList<>();
+		Employee employee3 = new Employee("3", 3000);
+		empleados.add(employee3);
+
+		/** Compruebo que employeeRepository devuelva una lista con un empleado **/
+		Mockito.when(employeeRepository.findAll()).thenReturn(empleados);
+
+		/** Compruebo que se paga a un empleado**/
+		assertThat(employeeManager.payEmployees()).isNotEqualTo(0);
+
+		/** Compruebo que se hace una llamada a bankService.pay **/
+		verify(bankService).pay(employee3.getId(), employee3.getSalary());
 
 	}
 
@@ -89,6 +106,27 @@ public class EmployeeManagerTest {
 	@Test
 	public void testPayEmployeesWhenSeveralEmployeeArePresent() {
 
+		List<Employee> employees = new ArrayList<>();
+
+		Employee empleado1 = new Employee("01", 1200);/** Creo los dos empleados **/
+		Employee empleado2 = new Employee("02", 1500);
+
+		employees.add(empleado1);/** Los añado a la coleccion de employees **/
+		employees.add(empleado2);
+
+		/** Compruebo que employeeRepository devuelve una colección con dos empleados **/
+		Mockito.when(employeeRepository.findAll()).thenReturn(employees);
+
+		/** Compruebo que se paga a los empleados**/
+		assertThat(employeeManager.payEmployees()).isNotEqualTo(0);
+
+		/** Verifico la interaccion (con verify) de que se hacen 2 invocaciones con las caracteristicas de pago de cada Employee **/
+		verify(bankService).pay(empleado1.getId(), empleado1.getSalary());
+		verify(bankService).pay(empleado2.getId(), empleado2.getSalary());
+
+		/** Verifico que no hay más interacciones con el mock de bankService  **/
+		verifyNoMoreInteractions(bankService);
+
 	}
 
 	/**
@@ -103,7 +141,27 @@ public class EmployeeManagerTest {
 	 */
 	@Test
 	public void testPayEmployeesInOrderWhenSeveralEmployeeArePresent() {
+		List<Employee> employees = new ArrayList<>();
 
+		Employee empleado1 = new Employee("01", 1200);/** Creo los dos empleados **/
+		Employee empleado2 = new Employee("02", 1500);
+
+		employees.add(empleado1);/** Los añado a la coleccion de employees **/
+		employees.add(empleado2);
+
+		/** Compruebo que employeeRepository devuelve una colección con dos empleados **/
+		Mockito.when(employeeRepository.findAll()).thenReturn(employees);
+
+		/**  Comprueba que cuando llamas a employeeManager.payEmployee pago a 2 empleados
+		 mediante Inorder e inOrder.verify **/
+		employeeManager.payEmployees();
+
+		InOrder inOrder = inOrder(bankService);
+		inOrder.verify(bankService).pay("01", 1200);
+		inOrder.verify(bankService).pay("02", 1500);
+
+		/** Por último, verifico que después de pagar no hay mas interacciones **/
+		verifyNoMoreInteractions(bankService);
 	}
 
 	/**
@@ -116,7 +174,26 @@ public class EmployeeManagerTest {
 	 */
 	@Test
 	public void testExampleOfInOrderWithTwoMocks() {
+		List<Employee> employees = new ArrayList<>();
 
+		Employee empleado1 = new Employee("01", 1200);/** Creo los dos empleados **/
+		Employee empleado2 = new Employee("02", 1500);
+
+		employees.add(empleado1);/** Los añado a la coleccion de employees **/
+		employees.add(empleado2);
+
+		/** Verifico que antes de hacer el pago bankService.pay para cada empleado se realiza la invocacion de employeeRepository.findAll  **/
+		when(employeeRepository.findAll()).thenReturn(employees);
+
+		employeeManager.payEmployees();
+
+		InOrder inOrder = inOrder(employeeRepository, bankService);
+		inOrder.verify(employeeRepository).findAll();
+		inOrder.verify(bankService).pay("01", 1200);
+		inOrder.verify(bankService).pay("02", 1500);
+
+		/** Por último, verifico que después de pagar no hay mas interacciones **/
+		verifyNoMoreInteractions(employeeRepository, bankService);
 	}
 
 
@@ -135,7 +212,31 @@ public class EmployeeManagerTest {
 	 */
 	@Test
 	public void testExampleOfArgumentCaptor() {
+		List<Employee> employees = new ArrayList<>();
 
+		Employee empleado1 = new Employee("01", 1200);/** Creo los dos empleados **/
+		Employee empleado2 = new Employee("02", 1500);
+
+		employees.add(empleado1);/** Los añado a la coleccion de employees **/
+		employees.add(empleado2);
+
+		/** Compruebo que employeeRepository devuelve una colección con dos empleados **/
+		Mockito.when(employeeRepository.findAll()).thenReturn(employees);
+
+		/**  Comprueba que cuando llamas a employeeManager.payEmployee pago a 2 empleados
+		 mediante Inorder e inOrder.verify **/
+		employeeManager.payEmployees();
+
+		verify(employeeRepository).findAll();
+
+		/** Luego utilizo los Captor: idCaptor y amountCaptor para capturar todos los id's y amounts **/
+		verify(bankService, times(2)).pay(idCaptor.capture(), amountCaptor.capture());
+
+		assertThat(idCaptor.getAllValues()).containsExactly("01", "02");
+		assertThat(amountCaptor.getAllValues()).containsExactly(1200.0, 1500.0);
+
+		/** Por último, verifico que después de pagar no hay mas interacciones **/
+		verifyNoMoreInteractions(bankService);
 	}
 
 	/**
@@ -149,7 +250,26 @@ public class EmployeeManagerTest {
 	 */
 	@Test
 	public void testEmployeeSetPaidIsCalledAfterPaying() {
+		List<Employee> employees = new ArrayList<>();
 
+		employees.add(toBePaid);/** Añado el spy toBePaid a la coleccion de employees **/
+
+		/** Compruebo que employeeRepository devuelve una colección con un empleado **/
+		Mockito.when(employeeRepository.findAll()).thenReturn(employees);
+
+		employeeManager.payEmployees();
+
+		/** Compruebo que se paga solo a un empleado **/
+		verify(bankService, times(1)).pay(idCaptor.capture(), amountCaptor.capture());
+		assertThat(idCaptor.getAllValues()).containsExactly("2");
+		assertThat(amountCaptor.getAllValues()).containsExactly(2000.0);
+
+		/** Verifico que la interaccion se realiza en el orden de bankService.pay con las caracteristicas del Employee toBePaid **/
+		InOrder inOrder = inOrder(bankService, toBePaid);
+		inOrder.verify(bankService).pay("2", 2000);
+
+		/** Verifico que se invoca tobePaid.setPaid true **/
+		inOrder.verify(toBePaid).setPaid(true);
 	}
 
 
@@ -167,7 +287,25 @@ public class EmployeeManagerTest {
 	 */
 	@Test
 	public void testPayEmployeesWhenBankServiceThrowsException() {
+		List<Employee> employees = new ArrayList<>();
 
+		employees.add(notToBePaid);/** Añado el spy toBePaid a la coleccion de employees **/
+
+		/** Compruebo que employeeRepository devuelve una colección con un empleado **/
+		Mockito.when(employeeRepository.findAll()).thenReturn(employees);
+
+		/** La exception a lanzar sera una RuntimeException **/
+		doThrow(new RuntimeException()).when(bankService).pay(anyString(), anyDouble());
+
+		employeeManager.payEmployees();
+
+		/** Compruebo que se paga solo a un empleado **/
+		verify(bankService, times(1)).pay(idCaptor.capture(), amountCaptor.capture());
+		assertThat(idCaptor.getAllValues()).containsExactly("1");
+		assertThat(amountCaptor.getAllValues()).containsExactly(1000.0);
+
+		/** Verifico que se invoca tobePaid.setPaid false **/
+		verify(notToBePaid).setPaid(false);
 	}
 
 	/**
@@ -185,6 +323,30 @@ public class EmployeeManagerTest {
 	 */
 	@Test
 	public void testOtherEmployeesArePaidWhenBankServiceThrowsException() {
+		List<Employee> employees = new ArrayList<>();
+
+		employees.add(notToBePaid);/** Añado el spy notToBePaid a la coleccion de employees **/
+		employees.add(toBePaid);/** Añado el spy toBePaid a la coleccion de employees **/
+
+		/** Compruebo que employeeRepository devuelve una colección con los dos empleados **/
+		Mockito.when(employeeRepository.findAll()).thenReturn(employees);
+
+		/** Creo un stub con encademaniento para 2 llamadas doThrow.doNothing-when para bankService.pay
+		 de modo que en la primera invocacion de pay (para notToBePaid) se lance una RuntimeException
+		 y en la segunda invocacion de pay (para toBePaid) no haga nada. **/
+		doThrow(new RuntimeException()).doNothing().when(bankService).pay(anyString(), anyDouble());
+		employeeManager.payEmployees();
+		verify(bankService, times(2)).pay(anyString(), anyDouble());
+
+		/** Verifico las interacciones (verify) sobre el spy notToBePaid primer mock de la coleccion
+		para el que lanza la RuntimeException y el spy toBePaid segundo mock de la coleccion **/
+		verify(notToBePaid).setPaid(false);
+		verify(toBePaid).setPaid(true);
+
+		/** Comprobando que si recibe el pago chequea la interaccion con el metodo setPaid a false y true respectivamente. **/
+		assertThat(toBePaid.isPaid()).isTrue();
+		assertThat(notToBePaid.isPaid()).isFalse();
+
 	}
 
 
@@ -204,7 +366,31 @@ public class EmployeeManagerTest {
 	 */
 	@Test
 	public void testArgumentMatcherExample() {
+		List<Employee> employees = new ArrayList<>();
 
+		employees.add(notToBePaid);/** Añado el spy notToBePaid a la coleccion de employees **/
+		employees.add(toBePaid);/** Añado el spy toBePaid a la coleccion de employees **/
+
+		/** Compruebo que employeeRepository devuelve una colección con los dos empleados **/
+		Mockito.when(employeeRepository.findAll()).thenReturn(employees);
+
+		/** Creo un stub con encademaniento para 2 llamadas doThrow-when empleando argThat  **/
+		Mockito.doThrow(new RuntimeException()).when(bankService).pay(argThat(s -> s.equals("1")), anyDouble());
+		Mockito.doNothing().when(bankService).pay(argThat(s -> s.equals("2")), anyDouble());
+
+		employeeManager.payEmployees();
+
+		/** Verifico las interacciones (verify) sobre el spy notToBePaid primer mock de la coleccion
+		para el que se lanza la RuntimeException y el spy toBePaid **/
+		Mockito.verify(bankService).pay("1", 1000.0);
+		Mockito.verify(bankService).pay("2", 2000.0);
+
+		/** Compruebo la interaccion con el metodo setPaid a false y a true respectivamente **/
+		Mockito.verify(notToBePaid).setPaid(false);
+		Mockito.verify(toBePaid).setPaid(true);
+
+		assertThat(toBePaid.isPaid()).isTrue();
+		assertThat(notToBePaid.isPaid()).isFalse();
 	}
 
 }
